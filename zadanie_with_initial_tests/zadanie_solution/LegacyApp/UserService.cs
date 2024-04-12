094b348414161d1c484efa28f933867196c24463
s20209 <s20209@pjwstk.edu.pl>
2024-04-12 20:52:23 +0200

    more extraction

--- zadanie_with_initial_tests/zadanie_solution/LegacyApp/UserService.cs
+++ zadanie_with_initial_tests/zadanie_solution/LegacyApp/UserService.cs
@@ -27,24 +27,7 @@ namespace LegacyApp{
                 LastName = lastName
             };
             
-            if (client.Type == "VeryImportantClient"){
-                user.HasCreditLimit = false;
-            }
-            else if (client.Type == "ImportantClient"){
-                using (var userCreditService = new UserCreditService()){
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else{
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
-            }
+            SetCreditLimit(user, client);
             
             if (user.HasCreditLimit && user.CreditLimit < 500){
                 return false;
@@ -54,18 +37,36 @@ namespace LegacyApp{
             return true;
         }
         private static bool validateName(string firstName, string lastName){
-        return !string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName);
-    }
+            return !string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName);
+        }
+        private static bool validateEmail(string email){
+            return email.Contains("@") && email.Contains(".");
+        }
 
-    private static bool validateEmail(string email){
-        return email.Contains("@") && email.Contains(".");
-    }
+        private static int calculateAge(DateTime dateOfBirth){
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
+                age--;
+            return age;
+        }
 
-    private static int calculateAge(DateTime dateOfBirth){
-        var now = DateTime.Now;
-        int age = now.Year - dateOfBirth.Year;
-         if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-         return age;
-    }
+        private static void SetCreditLimit(User user, Client client)
+        {
+            using (var userCreditService = new UserCreditService())
+            {
+                int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                if (client.Type == "VeryImportantClient")
+                {
+                    user.HasCreditLimit = false;
+                }
+                else
+                {
+                    creditLimit = client.Type == "ImportantClient" ? creditLimit * 2 : creditLimit;
+                    user.CreditLimit = creditLimit;
+                    user.HasCreditLimit = true;
+                }
+            }
+        }
     }
 }
